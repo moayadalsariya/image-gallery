48ea27e436d5005101ffd33f38b959518d794630
Moayad Alsariya <vvmoyadvv@gmail.com>
2021-05-23 14:47:06 +0300

    Add files via upload

---
+++ create.php
@@ -0,0 +1,108 @@
+<?php require_once "./views/paritals/header.php"; ?>
+
+<?php
+/** @var \PDO */
+require_once "./database.php";
+$errors = [];
+function generateRandomString($length = 10)
+{
+    return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
+}
+function slugify($text)
+{
+    // replace all non letters or digits by _
+    $text = preg_replace('/\W+/', '_', $text);
+
+    // trim and lowercase
+    $text = strtolower(trim($text, '_'));
+
+    return $text;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = htmlspecialchars($_POST["title"], ENT_QUOTES, "utf-8");
+    $desc = htmlspecialchars($_POST["desc"], ENT_QUOTES, "utf-8");
+    $image = "";
+    if (empty($title)) {
+        array_push($errors, "gallery Title is require");
+    }
+    if (empty($desc)) {
+        array_push($errors, "gallery Desc is require");
+    }
+    // check there is no file uploaded
+    if ($_FILES['image']['name'] == '') {
+        array_push($errors, "gallery Image is require");
+    }
+    // check if images dir is not exist
+    if (!file_exists("./images")) {
+        mkdir("./images");
+    }
+    // check if input filed is selected
+    if (isset($_FILES['image'])) {
+        $file_name = $_FILES['image']['name'];
+        $file_size = $_FILES['image']['size'];
+        $file_tmp = $_FILES['image']['tmp_name'];
+        $file_type = $_FILES['image']['type'];
+        $image = "images/" . generateRandomString() . $file_name;
+        $image = str_replace(' ', '_', $image);
+        $extensions = array("jpeg", "jpg", "png");
+        $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+        if (in_array($ext, $extensions) === false) {
+            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+        }
+        if ($file_size > 2097152) {
+            $errors[] = 'File size must be excately 2 MB';
+        } else {
+            move_uploaded_file($file_tmp, $image);
+        }
+    }
+
+
+    if (empty($errors)) {
+        $statment = $pdo->prepare("INSERT INTO gallery (title,description,image)
+        VALUES (:title,:desc,:image);");
+        $statment->bindValue(':title', $title);
+        $statment->bindValue(':desc', $desc);
+        $statment->bindValue(':image', $image);
+        $statment->execute();
+        header("Location: ./index.php");
+        exit();
+    }
+}
+?>
+
+<div class="container w-50">
+    <div class="py-5">
+
+        <?php
+        if (!empty($errors)) {
+            foreach ($errors as $error) { ?>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?php echo $error; ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+        <?php    }
+        }
+        ?>
+
+        <h3 class="text-center">Upload image</h3>
+        <form method="POST" enctype="multipart/form-data">
+            <div class="form-floating mb-3">
+                <input type="text" name="title" class="form-control" id="floatingInput" placeholder="title">
+                <label for="floatingInput">Title</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="text" name="desc" class="form-control" id="floatingInput" placeholder="desc">
+                <label for="floatingInput">desc</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="file" name="image" class="form-control" id="floatingInput" placeholder="image">
+                <label for="floatingInput">image</label>
+            </div>
+            <button type="Submit" class="btn btn-lg btn-success">Submit</button>
+        </form>
+    </div>
+</div>
+
+
+
+<?php require_once "./views/paritals/footer.php"; ?>
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php require_once "./views/paritals/header.php"; ?>
+
+<?php
+/** @var \PDO */
+require_once "./database.php";
+
+$statment = $pdo->prepare("SELECT * FROM gallery;");
+$statment->execute();
+$prodcut = $statment->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_GET['q'])) {
+  $q = htmlspecialchars($_GET["q"], ENT_QUOTES, "utf-8");
+
+  $statment = $pdo->prepare("SELECT * FROM gallery WHERE title LIKE :title ORDER BY created_date;");
+  $statment->bindValue(':title',"%$q%");
+  $statment->execute();
+  $prodcut = $statment->fetchAll(PDO::FETCH_ASSOC);
+}
+
+?>
+<main>
+
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Image Gallery CRUD</h1>
+        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+        <p>
+          <a href="./create.php" class="btn btn-lg btn-success my-2">Upload Image</a>
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+      <form action="" method="get">
+        <div class="input-group mb-3 w-50 mx-auto">
+          <input type="text" value="<?php if(isset($q)){echo $q;} ?>" name='q' class="form-control" placeholder="Search for image" aria-label="Username" aria-describedby="basic-addon1">
+          <button class="btn-primary">Search</button>
+        </div>
+      </form>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <?php foreach ($prodcut as $item) { ?>
+          <div class="col">
+            <div class="card shadow-sm">
+              <img src=<?php echo $item["image"]; ?> class="bd-placeholder-img card-img-top" width="100%" alt="" style="object-fit: cover;">
+
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $item["title"]; ?></h5>
+                <p class="card-text"><?php echo $item["description"]; ?></p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <form action="delete.php" method="post">
+                      <input type="hidden" name="id" value="<?php echo $item["id"]; ?>">
+                      <button type="submit" class="btn btn-danger btn-sm btn-outline-secondary text-white">Delete</button>
+                    </form>
+
+                    <a href="update.php?id=<?php echo $item["id"]; ?>" class="btn btn-sm btn-outline-secondary">Edit</a>
+                  </div>
+                  <small class="text-muted"><?php echo $item["created_date"]; ?></small>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php  }  ?>
+
+      </div>
+    </div>
+  </div>
+
+</main>
+
+
+<?php require_once "./views/paritals/footer.php"; ?>
